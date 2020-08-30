9b132d40d03d3f9ab0c9d7e9f2e0a884ca9bd660
fidesetratio <patartimotiustambunan@gmail.com>
2020-08-30 12:29:00 +0700

    Update filename.sh

--- scripts/filename.sh
+++ scripts/filename.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
 echo "File Name"
+
+if [ -d "../../scripts-linux" ]
+then
+        echo "ada nih ini menggunakan if else"
+else
+        echo "tidak ada ini"
+fi
+
+[ -d "../scripts-linux" ]
+
+echo "mengunakan scripts linux dengan parameter $?"
+
+[ -d "../../scripts-linux" ]
+
+echo "menggunakan scripts linux dengan parameter $?"
+
+
